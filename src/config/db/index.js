2629686f4bc364e1b1e06377230bfaec99c314fb
HaNhien919 <hanhien919@gmail.com>
2024-02-28 19:21:46 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect() {
 	try {
-		await mongoose.connect('mongodb://localhost:27017/db_domin_dev');
+		await mongoose.connect('mongodb+srv://24nguyenphat00:27072004phatZ@cluster0.7pcqw0h.mongodb.net/db_domin_dev');
 		console.log('Connected successfully!');
 	} catch (error) {
 		console.log('Error connecting');
